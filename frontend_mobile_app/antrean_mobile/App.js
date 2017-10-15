abed2665843661421a4cd8634b8729d5a69cf3c3
Agustinus Wikrama <agustinuswikrama@gmail.com>
2017-10-15 10:44:20 +0700

    Preparation of front end mobile application

---
+++ frontend_mobile_app/antrean_mobile/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './app/components/LoginScreen';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <LoginScreen/>
+    );
+  }
+}
